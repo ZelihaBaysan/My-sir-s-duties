d19926f15bbee9e6bad9d8cf505d29a0ca81196c
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2023-12-17 14:12:13 +0300

    burdaVeriDahaNet

---
+++ dorduncu-soru/branch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int generateRandom()
+{
+    return rand() % 6;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    int mermi[6] = {0, 0, 0, 0, 0, 0};
+    int mermiKonumu = generateRandom();
+    mermi[mermiKonumu] = 1;
+    int win1 = 0, win2 = 0, oyun = 0;
+   
+
+    while (oyun < 10000000)
+    {
+       
+        if (mermiKonumu % 2 == 0)
+        {
+            win1++;
+        }
+        else
+        {
+            win2++;
+        }
+
+        oyun++;
+
+        mermiKonumu = generateRandom();
+        mermi[mermiKonumu] = 1; 
+    }
+
+    printf("%d oyunda\nBirinci oyuncu: %d\nIkinci oyuncu: %d\nkadar kazandi\n", oyun, win1, win2);
+
+    return 0;
+}
