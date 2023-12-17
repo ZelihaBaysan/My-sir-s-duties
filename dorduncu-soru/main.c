235b0a17c07643995e3bc1bfcdde07dfa3e951d2
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2023-12-17 13:58:16 +0300

    bitti.

--- dorduncu-soru/main.c
+++ dorduncu-soru/main.c
@@ -2,25 +2,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-int random()
-	{
-		srand(time(NULL));
-		int random=rand() % 6;
-		return random;
-		
-	}
+int generateRandom()
+{
+    return rand() % 6;
+}
 
-int main() {
+int main()
+{
+    srand(time(NULL));
 
-	int mermi[6]={0,0,0,0,0,0};
-	int mermiKonumu= random();
-    mermi[mermiKonumu]=1;
+    int mermi[6] = {0, 0, 0, 0, 0, 0};
+    int mermiKonumu = generateRandom();
+    mermi[mermiKonumu] = 1;
+    int win1 = 0, win2 = 0, oyun = 0;
+    int i;
 
-	int i;
- 	for(i=0;i<6;i++)
- 	{
- 		printf("%d", mermi[i]);
-	 }
-    			
-	return 0;
+    while (oyun < 10)
+    {
+        printf("Mermi Durumu: ");
+        for (i = 0; i < 6; i++)
+        {
+            printf("%d ", mermi[i]);
+        }
+        printf("\n");
+
+        if (mermiKonumu % 2 == 0)
+        {
+            win1++;
+        }
+        else
+        {
+            win2++;
+        }
+
+        oyun++;
+
+        mermiKonumu = generateRandom();
+        mermi[mermiKonumu] = 1;
+    }
+
+    printf("%d oyunda\nBirinci oyuncu: %d\nIkinci oyuncu: %d\nkadar kazandi\n", oyun, win1, win2);
+
+    return 0;
 }
