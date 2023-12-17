762efbb76d3c3c941e3da651780826e5b41ddce2
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2023-12-17 13:45:53 +0300

    iyigidiom

--- dorduncu-soru/main.c
+++ dorduncu-soru/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() 
-{
-    
-    return 0;
+int random()
+	{
+		srand(time(NULL));
+		int random=rand() % 6;
+		return random;
+		
+	}
+
+int main() {
+
+	int mermi[6]={0,0,0,0,0,0};
+	int mermiKonumu= random();
+    mermi[mermiKonumu]=1;
+
+	int i;
+ 	for(i=0;i<6;i++)
+ 	{
+ 		printf("%d", mermi[i]);
+	 }
+    			
+	return 0;
 }
-
