a7138544432f6f98fcede61e01db5f7c85c45711
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2023-12-25 01:37:57 +0300

    yapamadım ya

--- altinci-soru/altinci.c
+++ altinci-soru/altinci.c
@@ -4,26 +4,41 @@
 
 int generateRandom()
 {
-    return rand() % 6; 
+    return rand() % 6;
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
 
     int switchh[6] = {0, 0, 0, 0, 0, 0};
     int acikSwitch = generateRandom();
     switchh[acikSwitch] = 1;
     int sayac = 0;
+    const int simulasyon = 1000000;
+    float sonuc = 0;
+	int i = 0;
+    for (i; i < simulasyon; i++)
+    {
+        while (!(switchh[0] == 0 && switchh[1] == 0 && switchh[2] == 0 &&
+                 switchh[3] == 0 && switchh[4] == 0 && switchh[5] == 0))
+        {
+            int random = generateRandom();
 
-    while (!(switchh[0] == 1 && switchh[1] == 1 && switchh[2] == 1 && switchh[3] == 1 && switchh[4] == 1 && switchh[5] == 1))
-{
+            if (switchh[random] == 1)
+            {
+                switchh[random] = 0;
+            }
+            else
+            {
+                switchh[random] = 1;
+            }
 
-        int acikSwitch = generateRandom();
-        switchh[acikSwitch] = 1;
-        
-        sayac++;
+            sayac++;
+        }
     }
 
-    printf("%d seferde tum switchler kapali olur\n", sayac);
+    sonuc =(float)sayac/simulasyon;
+    printf("Ortalama %f seferde 6 lamba da acik olur\n", sonuc);
     return 0;
 }
